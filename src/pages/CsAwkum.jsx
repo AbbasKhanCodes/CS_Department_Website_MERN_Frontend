d12240dbcc0c0fba92c4ab96c9b5fbc61a341ee7
AbbasKhanCodes <abbas.codes@gmail.com>
2023-07-12 08:01:47 +0500

    Corrected Import of News & Events in CsAwkum.jsx

--- src/pages/CsAwkum.jsx
+++ src/pages/CsAwkum.jsx
@@ -1,6 +1,6 @@
 import TopSlider from "./TopSlider";
 import WelcomeHeading from "../components/WelcomeHeading/WelcomeHeading";
-import NewsAndEvents from "../components/NewsAndEvents";
+import NewsAndEvents from "../components/NewsAndEvents/NewsAndEvents";
 import ChairmanMessage from "../components/ChairmanMessage/ChairmanMessage";
 import FacultySlider from "../components/FacultySlider/FacultySlider";
 import CardsSlider from "../components/CardSlider/CardsSlider";
