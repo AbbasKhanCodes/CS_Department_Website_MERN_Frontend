1618eb9e2195ee38d3e296a4e0fb84c10a5514d7
AbbasKhanCodes <abbas.codes@gmail.com>
2023-07-30 07:48:45 +0500

    - Popup-admission is added. - Admission section is changed to popup. - Footer background color is correct. - Applied white background color to Programs sections. - Changed titles color of Faculty & Projects to light yellow.

--- src/components/Admissions/Admissions.jsx
+++ src/components/Admissions/Admissions.jsx
@@ -4,23 +4,23 @@ const Admissions = () => {
   return (
     <>
       <div className={`${Styles.container}`}>
-        <div className={`${Styles.backgroundImg}`}>
-          <div className={`${Styles.insideBackgroundImg}`}></div>
-        </div>
         <div className={`${Styles.content}`}>
-          <h2>
-            Admissions <span>OPEN</span>{" "}
-          </h2>
+          <span className={`${Styles.admissionAlert}`}>
+            ADMISSIONS ALERT FALL-2023
+          </span>
+
           <h3>
-            Make your future, <span>Get Enrolled</span>
+            Admissions <span>OPEN.</span> Make your future,{" "}
+            <span>Get Enrolled</span>
           </h3>
           <p>
             Grab this opportunity. Enrolled in graduate and weekend programs of
-            Abdul Wali Khan University Mardan Grab this opportunity. Enrolled in
-            graduate and weekend programs of Abdul Wali Khan University Mardan
+            Abdul Wali Khan University Mardan
           </p>
           <a href="https://admissions.awkum.edu.pk/">
-            <button className="btn-primary btn-lg">Click to apply</button>
+            <button className={`${Styles.btnAction} btn-primary btn-lg`}>
+              Click to apply
+            </button>
           </a>
         </div>
       </div>
--- src/pages/CsAwkum.jsx
+++ src/pages/CsAwkum.jsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { createPortal } from "react-dom";
+
 import TopSlider from "../components/TopSlider/TopSlider";
 import WelcomeHeading from "../components/WelcomeHeading/WelcomeHeading";
 import Events from "../components/NewsAndEvents/NewsAndEvents";
@@ -6,14 +9,24 @@ import Projects from "../components/Projects/Projects";
 import Programs from "../components/Programs/Programs";
 import ProgramsMsPhd from "../components/Programs/ProgramsMsPhd";
 import Footer from "../components/Footer/Footer";
+import Popup from "../components/Popup/Popup";
 
 function CsAwkum() {
+  const [popupOpen, setPopupOpen] = useState(true);
+  const handleCloseBtn = () => {
+    setPopupOpen(false);
+  };
+
   return (
     <>
+      {/* Popup-admission */}
+      {popupOpen &&
+        createPortal(<Popup onClose={handleCloseBtn} />, document.body)}
+
       {/* Top Slider */}
       <TopSlider />
 
-      {/*  */}
+      {/* Welcome Heading */}
       <WelcomeHeading />
 
       {/* News & Events */}
