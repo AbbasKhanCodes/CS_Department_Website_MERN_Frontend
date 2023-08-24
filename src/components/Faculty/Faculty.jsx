f87241c241bdb01b280773a7e03f9138e115eae2
AbbasKhanCodes <abbas.codes@gmail.com>
2023-08-24 21:19:22 +0500

    - add slider in faculty section.
    - 8, 8 faculty are in each slide.

--- src/components/Faculty/Faculty.jsx
+++ src/components/Faculty/Faculty.jsx
@@ -2,12 +2,26 @@ import Styles from "./FacultyStyle.module.css";
 
 import Card from "./Card";
 import { faculty } from "./FacultyStaff";
+import { facultyWithoutRole } from "./FacultyStaff";
 
 // Import capitilize words
 import { capitalizeFirstLetter } from "../../utilities/utilities";
 
 import { motion } from "framer-motion";
 import { useEffect } from "react";
+
+// Slider
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-fade";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+// import required modules
+import { Autoplay, EffectFade, Pagination, Navigation } from "swiper/modules";
 
 // Animation
 let animate = {
@@ -49,30 +63,80 @@ const Faculty = () => {
           </div>
         </div>
 
-        <div className={Styles.faculty_component}>
-          <div className={Styles.faculty_tree}>
-            {faculty.map((person) => (
-              <div key={person.name.fullName} className={Styles.faculty_person}>
-                <img
-                  src={"../images/faculty/" + person.imageSrc}
-                  alt="Faculty Staff"
-                />
-                <div className={Styles.faculty_person_text_box}>
-                  <div>
-                    <h2>
-                      {person.name.title}
-                      {capitalizeFirstLetter(person.name.fullName)}
-                    </h2>
-                    <p>{person.academicTitle}</p>
-                    <p>{person.role}</p>
-                    <p>{person.email}</p>
-                  </div>
+        <div className={`${Styles.wrapperFacultySlider}`}>
+          <Swiper
+            slidesPerView={1}
+            spaceBetween={30}
+            loop={true}
+            pagination={{
+              clickable: true,
+            }}
+            navigation={true}
+            modules={[Pagination, Navigation]}
+            className={`${Styles.mySwiperFaculty}`}
+          >
+            <SwiperSlide>
+              <div className={Styles.faculty_component}>
+                <div className={Styles.faculty_tree}>
+                  {faculty.map((person) => (
+                    <div
+                      key={person.name.fullName}
+                      className={Styles.faculty_person}
+                    >
+                      <img
+                        src={"../images/faculty/" + person.imageSrc}
+                        alt="Faculty Staff"
+                      />
+                      <div className={Styles.faculty_person_text_box}>
+                        <div>
+                          <h2>
+                            {person.name.title}{" "}
+                            {capitalizeFirstLetter(person.name.fullName)}
+                          </h2>
+                          <p>{person.academicTitle}</p>
+                          <p>{person.role}</p>
+                          <p>{person.email}</p>
+                        </div>
 
-                  {/* <img src="./icons/Arrow 8.svg" alt="arrow forward" /> */}
+                        {/* <img src="./icons/Arrow 8.svg" alt="arrow forward" /> */}
+                      </div>
+                    </div>
+                  ))}
                 </div>
               </div>
-            ))}
-          </div>
+            </SwiperSlide>
+
+            <SwiperSlide>
+              <div className={Styles.faculty_component}>
+                <div className={Styles.faculty_tree}>
+                  {facultyWithoutRole.map((person) => (
+                    <div
+                      key={person.name.fullName}
+                      className={Styles.faculty_person}
+                    >
+                      <img
+                        src={"../images/faculty/" + person.imageSrc}
+                        alt="Faculty Staff"
+                      />
+                      <div className={Styles.faculty_person_text_box}>
+                        <div>
+                          <h2>
+                            {person.name.title}{" "}
+                            {capitalizeFirstLetter(person.name.fullName)}
+                          </h2>
+                          <p>{person.designation}</p>
+                          <p>{person.email}</p>
+                          <br />
+                        </div>
+
+                        {/* <img src="./icons/Arrow 8.svg" alt="arrow forward" /> */}
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </SwiperSlide>
+          </Swiper>
         </div>
       </motion.div>
     </>
