22eb50d397bcdd5610604a980d68c3ffac0ca7b3
AbbasKhanCodes <abbas.codes@gmail.com>
2023-08-25 08:14:36 +0500

    - graduation image changed.

--- src/components/Graduation/Graduation.jsx
+++ src/components/Graduation/Graduation.jsx
@@ -1,20 +1,28 @@
 import Styles from "./Graduation.module.css";
 const Graduation = () => {
   return (
-    <div className={Styles.graduation}>
-      <img src="./background/graduationImage.jpg" alt="graduation" />
-      <div className={Styles.Graduation_heading_container}>
-        <h2 className={Styles.graduation_heading}>
-          "Department of Computer Science has long been a magnet for talent in
-          computing an unparalleled center of technological and scientific
-          innovation."
-        </h2>
-        <p className={Styles.graduation_paragraph}>
-          - Dr. Sher Afzal Khan Professor at the department of Computer Science
-          and Dean Faculty of Business & Economics.
-        </p>
+    <>
+      <div className={Styles.graduation}>
+        <div className={`${Styles.image}`}>
+          <img
+            src="./images/graduation/agraduationImage.webp"
+            alt="graduation"
+          />
+        </div>
+
+        <div className={Styles.Graduation_heading_container}>
+          <h2 className={Styles.graduation_heading}>
+            "Department of Computer Science has long been a magnet for talent in
+            computing an unparalleled center of technological and scientific
+            innovation."
+          </h2>
+          <p className={Styles.graduation_paragraph}>
+            - Dr. Sher Afzal Khan Professor at the department of Computer
+            Science and Dean Faculty of Business & Economics.
+          </p>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
